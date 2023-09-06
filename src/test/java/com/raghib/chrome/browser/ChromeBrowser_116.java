42b5b5f185cff636fdb97d1061b286d0133da0e4
usmaniraghib <javaprogrammers786@gmail.com>
2023-09-06 15:42:48 +0530

    added

--- src/test/java/com/raghib/chrome/browser/ChromeBrowser_116.java
+++ src/test/java/com/raghib/chrome/browser/ChromeBrowser_116.java
@@ -1,36 +1,47 @@
 package com.raghib.chrome.browser;
 
+/**
+Question - Without WebDriverManager in pom dependency firefox and edge is running or no need to check.
+Answer - Without WebDriverManager, we are able to run the script in [Chrome, Firefox and Edge Browser].
+ 
+You must download chromedriver from below location for chrome version - 116 onwards.
+https://googlechromelabs.github.io/chrome-for-testing/
+ */
+
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import com.raghib.selenium.BaseClass;
 
-public class ChromeBrowser_116 {
+public class ChromeBrowser_116 extends BaseClass {
 
-	public static ChromeOptions chromeOptions;
 	public static WebDriver driver;
+	public static String browserName = "chrome";
+	public static String browserVersion = "116";
 
 	public static String url = "https://www.google.com";
 	public static String googleSearchBox = "//*[@id='APjFqb']";
 	public static String linkPath = "//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/a/h3";
 
-	public static void main(String[] args) throws InterruptedException {
-		// WAY-1 [THIS WILL WORKING FROM SELENIUM-JAVA VERSION 4.10.0]
-//		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver-116\\chromedriver.exe");
-//		driver = new ChromeDriver();
-
-		// WAY-2 [THIS WILL WORKING FROM SELENIUM-JAVA VERSION 4.11.0]
-		chromeOptions = new ChromeOptions();
-		chromeOptions.setBrowserVersion("116");
-		driver = new ChromeDriver(chromeOptions);
-
+	public static void main(String[] args) {
+		// Chrome Browser
+		driver = BaseClass.getDriver(browserName, browserVersion);		
+		//Firefox Browser
+//		driver = BaseClass.getDriver(browserName, browserVersion);		
+		//Edge Browser
+//		driver = BaseClass.getDriver(browserName, browserVersion);
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
 		driver.manage().window().maximize();
+		//driver.manage().window().minimize();
+		//driver.manage().window().fullscreen();
+		
 		driver.get(url);
 
 		WebElement element = driver.findElement(By.xpath(googleSearchBox));
@@ -46,10 +57,6 @@ public class ChromeBrowser_116 {
 		WebDriverWait wait1 = new WebDriverWait(driver, duration);
 		wait1.until(ExpectedConditions.elementToBeClickable((By.xpath(linkPath)))).click();
 
-		Thread.sleep(5000);
-
-		driver.quit();
-
+		//BaseClass.quitDriver();
 	}
-
 }
